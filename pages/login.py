2b0c13fc426524f9b9eaf57227aeaadf6424724b
VippadapuPujitha <vippadapupujitha12@gmail.com>
2026-02-12 18:33:28 +0530

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -1,24 +1,29 @@
 import streamlit as st
+
 st.markdown("""
 <style>
-[data-testid="stSidebar"] {
-    display: none;
-}
+[data-testid="stSidebar"] { display: none; }
 </style>
 """, unsafe_allow_html=True)
 
 st.title("Login")
 
-name = st.text_input("Name")
+if "users" not in st.session_state:
+    st.session_state.users = {}
+
+email = st.text_input("Email ID")
 password = st.text_input("Password", type="password")
 
 if st.button("Login"):
-    if name and password:
-        st.session_state.logged_in = True
-        st.switch_page("pages/app.py")
-    else:
+    if not email or not password:
         st.error("Please fill all fields")
+    elif email not in st.session_state.users:
+        st.error("Email not registered. Please register.")
+    elif st.session_state.users[email]["password"] != password:
+        st.error("Incorrect password")
+    else:
+        st.success("Login successful!")
+        st.session_state.logged_in = True
+        st.session_state.current_user = email
+        st.switch_page("pages/app.py")
 
-st.markdown("Don't have an account?")
-if st.button("Register"):
-    st.switch_page("pages/register.py")
